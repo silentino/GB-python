04e33d43ba126c8b0b2658900249003429e2f284
silentino <68819789+silentino@users.noreply.github.com>
2020-07-26 21:56:25 +0500

    Update hw01_02.py

--- hw01_02.py
+++ hw01_02.py
@@ -1,5 +1,5 @@
 user_time = int(input("Введите время в секундах >>>"))
-hour = user_time // 360
-minute = (user_time - (360 * hour)) // 60
-second = (user_time - (360 * hour) - (60 * minute))
+hour = user_time // 3600
+minute = (user_time - (3600 * hour)) // 60
+second = (user_time - (3600 * hour) - (60 * minute))
 print(f"Время в формате чч:мм:сс >>> {hour:02}:{minute:02}:{second:02}")
